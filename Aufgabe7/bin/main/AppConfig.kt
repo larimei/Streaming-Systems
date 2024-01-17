8efa1e0ae1e5821765eb97e3390cae54b995b320
Lara <lara.maria.meister@hs-furtwangen.de>
2024-01-17 10:14:21 +0100

    ex7

---
+++ Aufgabe7/bin/main/AppConfig.kt
@@ -0,0 +1,4 @@
+object AppConfig {
+    const val TOPIC = "traffic_data"
+    const val TOPIC_KEY = "data"
+}
---
+++ Aufgabe7/bin/main/consumer/ConsumerConfig.kt
@@ -0,0 +1,6 @@
+package consumer
+
+object ConsumerConfig {
+    const val TIME_WINDOW = 30
+    const val KM_FACTOR = 3.6
+}
